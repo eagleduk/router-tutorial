7bce0f1a53ac7ab2d0f31f30144a7c4d469cf12a
eagleduk <eagleduk@gmail.com>
2020-11-03 21:40:23 +0900

    Router 설치환경, 맛보기

---
+++ src/About.js
@@ -0,0 +1,12 @@
+function About() {
+    return (
+        <div>
+            <h1>소개</h1>
+            <p>
+                이 곳은 리액트 라우터 기초를 실습해보는 예제 프로젝트 입니다.
+            </p>
+        </div>
+    )
+}
+
+export default About;
